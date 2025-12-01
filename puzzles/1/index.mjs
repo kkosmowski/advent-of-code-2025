f9fe6c1667c7d13a6ad4e104a87f838e3665e793
Krzysztof Kosmowski <krzysztof.kosmowski@tsh.io>
2025-12-01 13:54:26 +0100

    puzzle: 1, part A

--- puzzles/1/index.mjs
+++ puzzles/1/index.mjs
@@ -6,8 +6,6 @@ import read from '../../helpers/read.mjs';
 const options = { puzzle: 1, split: true };
 
 const inputSm = 'input_sm.txt';
-const inputMd = 'input_md.txt';
-const inputLg = 'input_lg.txt';
 const realInput = 'input.txt';
 
 function result() {
@@ -23,24 +21,80 @@ class Puzzle1 {
   #data;
 
   #read() {
-    this.#data = read(inputSm, options);
-    // this.#data = read(inputMd, options);
-    // this.#data = read(inputLg, options);
-    // this.#data = read(realInput, options);
+    // this.#data = read(inputSm, options);
+    this.#data = read(realInput, options);
   }
 
   partA() {
     this.#read();
 
-    const something = this.#a.getSomething();
+    const result = this.#a.getResult(this.#data);
 
-    // return result;
-    return 'yo makefile'
+    return result;
   }
 
   #a = {
-    getSomething: (data) => {
+    maxPoint: 99,
+    initialPoint: 50,
+    zeroPointedCount: 0,
+    getResult: (data) => {
+      let currentDialPoint = this.#a.initialPoint;
 
+      for (const dataPoint of data) {
+        currentDialPoint = this.#a.getNextDialPoint(dataPoint, currentDialPoint);
+
+        if (currentDialPoint === 0) {
+          this.#a.zeroPointedCount += 1;
+        }
+      }
+
+      return this.#a.zeroPointedCount;
+    },
+    /**
+     *
+     * @param {string} dataPoint
+     * @param {number} currentDialPoint
+     */
+    getNextDialPoint: (dataPoint, currentDialPoint) => {
+      const [direction, distance] = this.#a.getDirectionAndDistance(dataPoint);
+      let result;
+
+      switch (direction) {
+        case 'L': {
+          result = currentDialPoint - distance;
+          break;
+        }
+        case 'R': {
+          result = currentDialPoint + distance;
+          break;
+        }
+        default: {
+          throw new Error(`Unsupported direction: ${direction}`);
+        }
+      }
+
+      return this.#a.customModulo(result, this.#a.maxPoint);
+    },
+    /**
+     *
+     * @param dataPoint
+     * @return {['L' | 'R', number]}
+     */
+    getDirectionAndDistance: (dataPoint) => {
+      const direction = dataPoint.slice(0, 1);
+      const distance = Number(dataPoint.slice(1));
+      return [direction, distance];
+    },
+    customModulo: (value, mod) => {
+      let result = value;
+      if (result < 0) {
+        while (result < 0) {
+          result = result + (mod + 1);
+        }
+      } else {
+        result = result % (mod + 1);
+      }
+      return result;
     },
   }
 
